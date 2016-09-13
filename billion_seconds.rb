3890ccae50bcc2e0d455e56466064e3837f6c1b4
Development-Urquidi <pro.urquidi@gmail.com>
2016-09-13 17:14:46 -0500

    Add files via upload

---
+++ billion_seconds.rb
@@ -0,0 +1,8 @@
+class BillionSeconds
+
+  puts 'If I was born on: '
+  puts Time.mktime(1993,11,17)
+  puts 'then I will be one billion seconds old on: '
+  puts Time.mktime(1993,11,17) + 1000000000
+
+end
